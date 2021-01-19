ad09e2ffe0b3896d24ee16e7c8a59a1451ba34be
Karan Sanjeev <yo@alphaman.me>
2021-01-19 15:37:44 +0530

    Delete old list_todos file

--- l4-activerecord/list_todos.rb
+++
@@ -1,9 +0,0 @@
-require './connect_db'
-require './todo'
-
-connect_db!
-
-displayable_list = Todo.all.map { |todo| todo.to_displayable_string }
-
-puts displayable_list
-puts Todo.where(completed: true).to_displayable_list
