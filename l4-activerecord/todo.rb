79f2a34b5f345e0220eedebf0a3e22ebad354c1b
Karan Sanjeev <yo@alphaman.me>
2021-01-19 15:38:50 +0530

    Complete list todo task

--- l4-activerecord/todo.rb
+++ l4-activerecord/todo.rb
@@ -8,10 +8,29 @@ class Todo < ActiveRecord::Base
   def to_displayable_string
     display_status = completed ? '[X]' : '[ ]'
     display_date = due_today? ? nil : due_date
-    "#{display_status} #{todo_text} #{display_date}"
+    "#{id}. #{display_status} #{todo_text} #{display_date}"
   end
 
   def self.to_displayable_list
     all.map { |todo| todo.to_displayable_string }
+  end
+
+  def self.show_list
+    overdue_todos = where("due_date > ?", Date.today)
+    todays_todos = where(due_date: Date.today)
+    later_todos = where("due_date < ?", Date.today)
+
+    puts 'Overdue'
+    puts(overdue_todos.map { |todo| todo.to_displayable_string })
+
+    puts "\n"
+
+    puts 'Due Today'
+    puts(todays_todos.map { |todo| todo.to_displayable_string })
+
+    puts "\n"
+
+    puts 'Due Later'
+    puts(later_todos.map { |todo| todo.to_displayable_string })
   end
 end
